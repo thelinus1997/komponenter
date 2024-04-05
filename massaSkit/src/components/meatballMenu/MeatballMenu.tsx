74ecf00e9784c02d6dcb8abf63aa4641238f18ed
thelinus1997 <thelinus1997@gmail.com>
2024-04-05 15:05:34 +0200

    confetti bro

--- massaSkit/src/components/meatballMenu/MeatballMenu.tsx
+++ massaSkit/src/components/meatballMenu/MeatballMenu.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./meatballMenu.scss";
 import meatballSvg from "./meatballSvg.svg";
+import JSConfetti from "js-confetti";
 
 const numArr: number[] = [1, 2, 3, 4, 5];
 const MeatballMenu = () => {
@@ -11,24 +12,28 @@ const MeatballMenu = () => {
     setmenuVis(!menuVis);
   };
   const handleClick = (input: number) => {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti({
+      emojis: ["🤣", "😂", "🍆", "🦴"],
+      confettiNumber: 100,
+      confettiRadius: 1,
+      emojiSize: 75,
+    });
     setValue(input);
     setmenuVis(false);
   };
   return (
-    <div>
-      <h1>Change this number: {value}</h1>
-      <div className={`meatballMenu ${menuVis ? "visible" : ""}`}>
-        <div className="meatballButton" onClick={menuVisibility}>
-          <img src={meatballSvg} alt="" />
-        </div>
-        <ul className={`dropdown ${menuVis ? "visible" : ""}`}>
-          {numArr.map((index) => (
-            <li onClick={() => handleClick(numArr[index - 1])}>
-              {numArr[index - 1]}
-            </li>
-          ))}
-        </ul>
+    <div className={`meatballMenu ${menuVis ? "visible" : ""}`}>
+      <div className="meatballButton" onClick={menuVisibility}>
+        <img src={meatballSvg} alt="" />
       </div>
+      <ul className={`dropdown ${menuVis ? "visible" : ""}`}>
+        {numArr.map((index) => (
+          <li onClick={() => handleClick(numArr[index - 1])}>
+            {numArr[index - 1]}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
