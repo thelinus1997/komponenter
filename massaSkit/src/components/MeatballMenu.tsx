a4e6f5a7d8ce12d960ca0890ceb658f425921620
thelinus1997 <thelinus1997@gmail.com>
2024-04-05 09:49:48 +0200

    idespiseGIT

---
+++ massaSkit/src/components/MeatballMenu.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./meatballMenu.scss";
+const MeatballMenu = () => {
+  return (
+    <div className="tableStuff">
+      <table>
+        <tbody>
+          <tr>
+            <td>Change my color!</td>
+            <td>Change my opacity!</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default MeatballMenu;
