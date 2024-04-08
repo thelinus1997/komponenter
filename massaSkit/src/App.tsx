da57634317dd9019b7735d8055ac9207744dfc99
EllieBach <elisabeth.bach95@hotmail.com>
2024-04-08 10:27:20 +0200

    ajajaj

--- massaSkit/src/App.tsx
+++ massaSkit/src/App.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 import MeatballMenu from "./components/meatballMenu/MeatballMenu";
 import SearchBtn from "./components/SearchBtn/SearchBtn";
+import BentoMenu from "./components/BentoMenu/BentoMenu";
 const App = () => {
   return (
     <>
       <MeatballMenu />
       <SearchBtn />
+      <BentoMenu/>
     </>
   );
 };
